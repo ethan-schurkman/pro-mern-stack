1e7e3e4cf25c486223dad4ea35e67f2966f31229
esos04 <schurkman4@gmail.com>
2017-11-29 22:28:58 -0500

    add in fancy new issuefilter. Using forms

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -63,7 +63,9 @@ export default class IssueList extends React.Component {
   componentDidUpdate(prevProps) {
     const oldQuery = prevProps.location.query;
     const newQuery = this.props.location.query;
-    if (oldQuery.status === newQuery.status) {
+    if (oldQuery.status === newQuery.status
+        && oldQuery.effort_gte === newQuery.effort_gte
+        && oldQuery.effort_lte === newQuery.effort_lte) {
       return;
     }
     this.loadData();
@@ -125,7 +127,10 @@ export default class IssueList extends React.Component {
   render() {
     return (
       <div>
-        <IssueFilter setFilter={this.setFilter} />
+        <IssueFilter
+          setFilter={this.setFilter}
+          initFilter={this.props.location.query}
+        />
         <hr />
         <IssueTable issues={this.state.issues} />
         <hr />
